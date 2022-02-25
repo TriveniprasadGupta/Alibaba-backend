dea48675538ce23bd6281c7c2c2d474bdd071916
TriveniprasadGupta <iam.trivenigupta@gmail.com>
2022-02-25 10:47:45 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const connect = require("./config/db");
+const cors = require('cors')
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 const passport = require("./config/google-oauth");
 
